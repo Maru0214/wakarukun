ff3f5277af8eed6384d25f8bf5bd1446b2c864e9
Maru0214 <112690229+Maru0214@users.noreply.github.com>
2023-04-27 15:34:14 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { mdiPost } from "@mdi/js";
+</script>
+
+<template>
+  <VApp>
+    <VAppBar color="primary">
+      <template v-slot:prepend>
+        <VAppBarNavIcon color="on-primary" :icon="mdiPost" />
+      </template>
+      <v-app-bar-title>tpl-nuxt</v-app-bar-title>
+    </VAppBar>
+    <VMain>
+      <NuxtPage />
+    </VMain>
+  </VApp>
+</template>
