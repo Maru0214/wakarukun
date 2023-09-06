dcf3d70ab581ea96e3fa9e17cac2f1917497ed82
maru0214 <syunnsuke0323@gmail.com>
2023-09-06 17:58:03 +0900

    googleアカウント　認証　ついかしたかった

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="signup-form">
+    <p class="title">SNSアカウントで登録</p>
+    <ul class="signup-buttons">
+      <li>
+        <button class="google-button" @click="googleSignUp">
+          <i><GoogleLogo></GoogleLogo></i><span>Googleで登録</span>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GoogleLogo from "@/assets/svg/icons/google-logo.svg";
+const googleSignUp = () => {
+  const { googleSignUp } = useAuth();
+  googleSignUp();
+};
+</script>
