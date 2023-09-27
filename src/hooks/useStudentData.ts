830c20108c33a3b4b66432e883993e613ea5b33c
Maru0214 <112690229+Maru0214@users.noreply.github.com>
2023-09-28 01:15:16 +0900

    変数のやり取りを学びたい所存

--- src/hooks/useStudentData.ts
+++ src/hooks/useStudentData.ts
@@ -1,7 +1,12 @@
 import { doc, setDoc } from "firebase/firestore";
 
-type studentData = { mail: string; isWakaru: boolean };
+type studentData = {
+  mail: string;
+  isWakaru: boolean;
+};
+
 // (引数)なし
+let mailaddress = email;
 
 export function useStudentData() {
   const { $db } = useNuxtApp();
@@ -10,7 +15,7 @@ export function useStudentData() {
     const docRef = doc($db, "student", "test-user");
     // これでデータベースにデータを置き換える（入れ込む）
     const student: studentData = {
-      mail: "あどれすdesu",
+      mail: mailaddress,
       isWakaru: false,
     };
     await setDoc(docRef, student);
