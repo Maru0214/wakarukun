414b4a2ee98493938bbf820ccf8206d788c2d51e
Lynn-u-u <rin_soe@icloud.com>
2023-10-24 18:49:19 +0900

    画像をやめました

--- src/pages/teacher.vue
+++ src/pages/teacher.vue
@@ -31,53 +31,121 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="example">
-   <img src="../assets/img/haikei.svg">
-   <p>
+  <head>
+    <meta name="viewport" content="width=device-width"/>
+  </head>
+  <p class="title">わからない生徒</p>
+  
+  <p class="seitosu">
     {{ idListCount.wakaranai }}
-  </p>	
+    <div class="hito">
+    人
   </div>
+  </p>	
+  <h2>　</h2>
 
   <div class="pasento">
-    <!--{{青文字は数字が入る変数。}}-->
-    <br />
    {{(idListCount.wakaranai / (idListCount.wakaranai + idListCount.wakaru)) *
       100}}%
   </div>
+  
 </template>
 
 <style>
-.example {/*親div*/
-  position: relative;
-  text-align: center
-  }
 
-  .example p {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -ms-transform: translate(-50%,-50%);
-  -webkit-transform: translate(-50%,-50%);
-  transform: translate(-50%,-50%);
-  margin:0;
-  padding:0;
-  font-size: 18vh;
+/*共通スタイル*/
+.title {
+  margin-top: 5%;
+  text-align: center;
   color: #5F5F5F;
-  font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  /*文字の装飾は省略*/
-  }
+  font-family: vdl-logojrblack, sans-serif;
+  font-style: normal;
+  font-size: 350%;  
+}
 
-.example img {
-  width: 50%;
-  height:50%;
-  }
+.seitosu {
+  text-align: center;
+  color: #5F5F5F;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold',sans-serif;
+  font-style: normal;
+  font-size:1300%;  
+}
 
-  .pasento{
+.hito {
+  text-align: center;
+  color: #5F5F5F;
+  font-family:vdl-logojrblack, sans-serif;
+  font-style: normal;
+  font-size:30%; 
+  margin-top: -22%;
+  margin-left: 30%;
+}
+
+h2 {
     position: relative;
-    left: 44%;
-    margin-top: -10%;
-    font-size: 10vh;
+    padding: 0.8rem 0;
+    margin-bottom: 0.2rem;
+    border-bottom: 5px solid;
+    color: rgb(91, 90, 90);
+    font-weight: bold;
+    font-size: 26px;
+    text-align: center;
+}
+
+h2:before,
+h2:after {
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    content: "";
+    height: 0;
+    width: 0;
+}
+
+h2:before {
+    border: 16px solid;
+    border-color: transparent;
+    border-top-color: black;
+    margin-left: -16px;
+}
+
+h2:after {
+    border: 10px solid;
+    border-color: transparent;
+    border-top-color: white;
+    margin-left: -10px;
+}
+
+.example p {
+  position: absolute;
+  bottom: -13%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  margin: 0;
+  padding: 0;
+  font-size: 25vw;
   color: #5F5F5F;
-  font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold',sans-serif;
+}
+
+.pasento {
+    top: 40%;
+    margin-left: 43%;
+    font-size: 10vw;
+    color:#5F5F5F;
+    font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
   }
+
+/*
+.pasento {
+    margin-top: 20%;
+    text-align: center;
+    font-size: 10vw;
+    color:#5F5F5F;
+    font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  }
+}
+*/
   </style>
